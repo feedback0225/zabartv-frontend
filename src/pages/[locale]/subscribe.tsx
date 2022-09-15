81a8e60c43f30ebe6ddd6f561d3bf5f9278eef3d
Jeka Shiyan <ncringe31@gmail.com>
2022-09-15 11:27:05 +0300

    i18n with axios get query

--- src/pages/[locale]/subscribe.tsx
+++ src/pages/[locale]/subscribe.tsx
@@ -1,9 +1,19 @@
 import { Layout } from '@/components/Layout/Layout';
 import { Subscribe } from '@/screens/Subscribe/Subscribe';
-import { getStaticPaths, makeStaticProps } from '@/lib/getStatic';
+import { getI18nProps, getStaticPaths, makeStaticProps } from '@/lib/getStatic';
+import axios from 'axios';
 import type { NextPage } from 'next';
 
-const SubscribePage: NextPage = () => {
+export async function getStaticProps(ctx: any) {
+	const { data } = await axios.get('https://jsonplaceholder.typicode.com/users');
+	return { props: { data, ...(await getI18nProps(ctx)) } };
+}
+
+export { getStaticPaths };
+
+const SubscribePage: NextPage = ({ data }: any) => {
+	console.log(data);
+
 	return (
 		<Layout>
 			<Subscribe />
@@ -12,6 +22,3 @@ const SubscribePage: NextPage = () => {
 };
 
 export default SubscribePage;
-
-const getStaticProps = makeStaticProps(['common']);
-export { getStaticPaths, getStaticProps };
