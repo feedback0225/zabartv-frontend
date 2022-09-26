c8fd2180b6fed410d36291edf1778a57b02b0a32
Jeka Shiyan <ncringe31@gmail.com>
2022-09-26 18:03:35 +0300

    some fixes

--- src/components/screens/Movie/components/Player/Player.tsx
+++ src/components/screens/Movie/components/Player/Player.tsx
@@ -2,13 +2,13 @@
 import { ButtonBase } from '@/components/ButtonBase/ButtonBase';
 import { CloseIcon } from '@/icons';
 import { useLockedBody } from 'usehooks-ts';
-import { VideoJS } from './components/VideoJS/VideoJS';
+import { VideoPlayer } from './components/VideoPlayer/VideoPlayer';
 import { useTypedSelector } from '@/hooks/useTypedSelector';
 import { useTypedActions } from '@/hooks/useTypedActions';
-import classNames from 'classnames';
-import styles from './Player.module.scss';
 import { useEffect } from 'react';
 import { useWindowSize } from '@/hooks/useWindowSize';
+import classNames from 'classnames';
+import styles from './Player.module.scss';
 
 export const Player = () => {
 	const { isVisiblePlayer } = useTypedSelector((state) => state.player);
@@ -38,7 +38,7 @@ export const Player = () => {
 			<ButtonBase onClick={handleClosePlayer} className={styles.close}>
 				<CloseIcon />
 			</ButtonBase>
-			<VideoJS />
+			<VideoPlayer />
 		</div>
 	);
 };
--- src/components/screens/Movie/components/Player/components/VideoPlayer/VideoPlayer.tsx
+++ src/components/screens/Movie/components/Player/components/VideoPlayer/VideoPlayer.tsx
@@ -2,9 +2,8 @@ import { useEffect, useRef } from 'react';
 import { useTypedSelector } from '@/hooks/useTypedSelector';
 import videojs, { VideoJsPlayer } from 'video.js';
 import hotkeys from 'videojs-hotkeys';
-import 'video.js/dist/video-js.css';
 
-export const VideoJS = () => {
+export const VideoPlayer = () => {
 	const videoRef = useRef(null);
 	const playerRef = useRef<VideoJsPlayer | null>(null);
 
@@ -26,6 +25,14 @@ export const VideoJS = () => {
 				plugins: {
 					hotkeys,
 				},
+				controlBar: {
+					volumePanel: {
+						inline: false,
+					},
+					pictureInPictureToggle: false,
+					playToggle: true,
+				},
+
 				sources: [
 					{
 						src: 'https://appsignals.coderman.top/stream/index?path=/1/MH4D2psEpK5nt42q5pml-4pxvwe5wjaZ.mp4',
@@ -33,6 +40,8 @@ export const VideoJS = () => {
 					},
 				],
 			});
+
+			playerRef.current.play();
 		}
 		// eslint-disable-next-line react-hooks/exhaustive-deps
 	}, [videoRef]);
