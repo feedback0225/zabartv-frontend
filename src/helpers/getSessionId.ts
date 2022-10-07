d31382e3b0e05a21f62fbe06d6955f4e4bd88120
Jeka Shiyan <ncringe31@gmail.com>
2022-10-07 13:37:34 +0300

    fix session id"

--- src/helpers/getSessionId.ts
+++ src/helpers/getSessionId.ts
@@ -1,13 +1,11 @@
 export const getSessionId = () => {
-	// if (typeof window === 'undefined') {
-	//     return 'qweqweqwe'
-	// } else {
-	//     if(!window?.localStorage?.getItem('zabar_session_id')?.length) {
-	//         window.localStorage.setItem('zabar_session_id', String(Date.now()))
-	//     }
+	if (typeof window === 'undefined') {
+	    return 'qweqq'
+	} else {
+	    if(!window?.localStorage?.getItem('zabar_session_id')?.length) {
+	        window.localStorage.setItem('zabar_session_id', String(Date.now()))
+	    }
 
-	//     return window.localStorage.getItem('zabar_session_id')
-	// }
-
-	return 'qweqweqwe';
+	    return window.localStorage.getItem('zabar_session_id')
+	}
 };
