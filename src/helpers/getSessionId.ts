5605857a5a9a2a7106973e64f274f43f671515a8
Shiyan7 <57950265+Shiyan7@users.noreply.github.com>
2022-10-07 14:00:56 +0300

    Update getSessionId.ts

--- src/helpers/getSessionId.ts
+++ src/helpers/getSessionId.ts
@@ -1,13 +1,14 @@
 export const getSessionId = () => {
-	// if (typeof window === 'undefined') {
-	//     return 'qweqweqwe'
-	// } else {
-	//     if(!window?.localStorage?.getItem('zabar_session_id')?.length) {
-	//         window.localStorage.setItem('zabar_session_id', String(Date.now()))
-	//     }
+	 if (typeof window === 'undefined') {
+	     return 'qweqweqweqwq'
+	 } else {
+	     if(!window?.localStorage?.getItem('zabar_session_id')?.length) {
+	         window.localStorage.setItem('zabar_session_id', String(Date.now()))
+	     }
 
-	//     return window.localStorage.getItem('zabar_session_id')
-	// }
+	     return window.localStorage.getItem('zabar_session_id')
+	 }
 
-	return 'qweqweqwe';
+
+	
 };
