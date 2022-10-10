ec478d8cbbe0b8f86fd72f1b5d2eca3b739927af
Jeka Shiyan <ncringe31@gmail.com>
2022-10-10 12:31:13 +0300

    storybook tests

--- src/components/screens/Cabinet/components/CabinetInput/CabinetInput.stories.tsx
+++ src/components/screens/Cabinet/components/CabinetInput/CabinetInput.stories.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import { useState } from 'react';
 import { ComponentStory, ComponentMeta } from '@storybook/react';
 import { CabinetInput } from './CabinetInput';
 
@@ -7,14 +7,26 @@ export default {
 	component: CabinetInput,
 } as ComponentMeta<typeof CabinetInput>;
 
-const Template: ComponentStory<typeof CabinetInput> = () => {
+const DefaultInput: ComponentStory<typeof CabinetInput> = () => {
 	const [value, setValue] = useState<string>('aassmoilov@gmail.com');
 
 	return (
 		<div style={{ width: '350px' }}>
-			<CabinetInput label="Email" type="email" value={value} applyChanges={setValue} />
+			<CabinetInput label="Email" placeholder={value} type="email" value={value} applyChanges={setValue} />
 		</div>
 	);
 };
 
-export const Default = Template.bind({});
+export const Input = DefaultInput.bind({});
+
+const MaskInput: ComponentStory<typeof CabinetInput> = () => {
+	const [value, setValue] = useState<string>('11.11.1111');
+
+	return (
+		<div style={{ width: '350px' }}>
+			<CabinetInput mask='00.00.0000' label="Дата рождения" placeholder={value} value={value} applyChanges={setValue} />
+		</div>
+	);
+};
+
+export const InputWithMask = MaskInput.bind({});
