7d87607c2d7f15b8ec7adcf7908c0f99dd0d2b1d
AbdullayevAbdurashid <abdulllayevabdurashid@gmail.com>
2022-11-05 15:41:33 +0500

    Language change added to server

--- src/components/Header/components/Lang/Lang.tsx
+++ src/components/Header/components/Lang/Lang.tsx
@@ -5,7 +5,7 @@ import { useRouter } from 'next/router';
 import NextLink from 'next/link';
 import classNames from 'classnames';
 import styles from './Lang.module.scss';
-
+import { getSessionId } from '@/utils/getSessionId';
 interface LangProps {
 	className?: string;
 }
@@ -22,10 +22,20 @@ export const Lang: FC<LangProps> = ({ className }) => {
 	useOnClickOutside(langRef, () => setActive(false));
 
 	const items = [
-		{ icon: <RuIcon />, txt: 'Русский язык', locale: 'ru' },
-		{ icon: <CeIcon />, txt: 'Чеченский язык', locale: 'ce' },
+		{ icon: <RuIcon />, txt: 'Русский язык', locale: 'ru', code: 'ru-RU' },
+		//Я указал коды языков чтобы они синхронизировались в бэке
+		{ icon: <CeIcon />, txt: 'Чеченский язык', locale: 'ce', code: 'che_CHE' },
 	];
-
+	const setSessionid = (el: string) => {
+		const session_id = getSessionId();
+		//Я сделал апи вызов прямо здесь. потом его измените
+		//Делаем запрос на изменения языка. И все теперь пр запросах указывайе session_id категории придуд в выбранном языке
+		fetch(
+			`http://appsignals.coderman.top/api/v1/languages/index?lang=${el}&session_id=${session_id}&token=dev-token`
+		)
+			.then((response) => response.json())
+			.then((data) => console.log(data));
+	};
 	return (
 		<div
 			ref={langRef}
@@ -39,7 +49,7 @@ export const Lang: FC<LangProps> = ({ className }) => {
 						className={classNames(styles.item, locale === el.locale && styles.selected)}
 					>
 						<NextLink href={asPath} locale={el.locale}>
-							<a className={styles.link}>
+							<a onClick={() => setSessionid(el.code)} className={styles.link}>
 								{el.icon}
 								<span className={styles.txt}>{el.txt}</span>
 							</a>
--- src/utils/getSessionId.ts
+++ src/utils/getSessionId.ts
@@ -1,11 +1,23 @@
-export const getSessionId = () => {
-	if (typeof window === 'undefined') {
-		return 'qweqweqweqweqwe';
-	} else {
-		if (!window?.localStorage?.getItem('zabar_session_id')?.length) {
-			window.localStorage.setItem('zabar_session_id', String(Date.now()));
-		}
+import { getCookie, setCookie } from 'cookies-next';
 
-		return window.localStorage.getItem('zabar_session_id');
+// export const getSessionId = () => {
+// 	if (typeof window === 'undefined') {
+// 		return 'qweqweqweqweqwe';
+// 	} else {
+// 		if (!window?.localStorage?.getItem('zabar_session_id')?.length) {
+// 			window.localStorage.setItem('zabar_session_id', String(Date.now()));
+// 		}
+
+// 		return window.localStorage.getItem('zabar_session_id');
+// 	}
+// };
+export const getSessionId = () => {
+	//Проверяем есть ли session id
+	const cookie = getCookie('session_id');
+	//Если его нет создаем новый
+	if (!cookie) {
+		setCookie('session_id', Date.now(), { maxAge: 91000 });
+		return getCookie('session_id');
 	}
+	return cookie;
 };
